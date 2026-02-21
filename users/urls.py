309b3f80040663cbbc270c94d706fb4b3796fe83
AizatKarimzhanova <aizatkarimova00@gmail.com>
2026-02-21 11:55:16 +0600

    lesson7

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.reqister_view),
-    path('', views.auth_login_view, name='login'),
-    path('logout/', views.auth_logout_view),
+    path('register/', views.RegisterView.as_view()),
+    path('', views.AuthLoginView.as_view(), name='login'),
+    path('logout/', views.AuthLogoutView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,45 +1,81 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from users.forms import CustomUserForm
 from django.http import HttpResponse
+from django.views import generic
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
-def reqister_view(request):
-    if request.method == 'POST':
-        form = CustomUserForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-    else:
-        form = CustomUserForm()
-    return render(
-        request,
-        'register.html',
-        {
-            'form': form
-        }
-    )
+class RegisterView(generic.CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserForm
+
+    def get_success_url(self):
+        return reverse('login')
+    
+    
 
 
-def auth_login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('yaziki:yaziki_programirovanie')
-            #return redirect('/prog_lang/')
-    else:
-        form = AuthenticationForm()
-    return render(
-        request,
-        'login.html',
-        {
-            'form': form
-        }
-    )
-def auth_logout_view(request):
-    logout(request)
-    return redirect('/login/')
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = CustomUserForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("login")
+#     else:
+#         form = CustomUserForm()
+    
+#     return render(
+#         request,
+#         'register.html',
+#         {
+#             "form": form
+#         }
+#     )
+
+
+
+class AuthLoginView(LoginView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+
+    def get_success_url(self):
+        return reverse("yaziki:yaizki_programmirovanie")
+
+
+
+
+
+
+
+
+# def auth_login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('yaziki:yaizki_programmirovanie')
+#             #return redirect('/prog_lang/')
+#     else:
+#         form = AuthenticationForm()
+    
+#     return render(
+#         request,
+#         'login.html',
+#         {
+#             "form": form
+#         }
+#     )
+
+
+class AuthLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+
+
+# def auth_logout_view(request):
+#     logout(request)
+#     return redirect('/login/')
 
